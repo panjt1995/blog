8e8e7a3fe4a8f71d2030c5f2c4f8e872c54ff8e8
pan <pan@archivium.digital>
2026-01-06 23:53:58 +0100

    init

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '不求神';
+export const SITE_DESCRIPTION = '探索技术，服务生活 —— 一个全栈开发者的个人空间。';
